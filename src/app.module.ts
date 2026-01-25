5e9f5713cc409722461238e453e333827075cc2b
Mit <mit4sheth@gmail.com>
2026-01-26 01:04:29 +0530

    init

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
+import { AuthModule } from "./shared/auth/auth.module";
+import { HealthModule } from "./shared/health/health.module";
+import { StorageModule } from "./shared/storage/storage.module";
+import { ChannelsModule } from "./modules/channels/channels.module";
+import { ThreadsModule } from "./modules/threads/threads.module";
+import { MessagesModule } from "./modules/messages/messages.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    StorageModule,
+    HealthModule,
+    ChannelsModule,
+    ThreadsModule,
+    MessagesModule
+  ]
+})
+export class AppModule {}
+
